961f4b18e8ade5d99b19e5bc9d47c341ba0ea0e3
NeverW8 <carl.skantz@carlskantz.se>
2024-07-20 17:35:34 +0200

    build script, gitignore added and sum file removed

---
+++ scripts/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+export GOPATH=$(go env GOPATH)
+export PATH=$GOPATH/bin:$PATH
+
+run_tests() {
+  echo "Running tests..."
+  go test ./...
+}
+
+run_verification() {
+  echo "Running golint..."
+
+  go install golang.org/x/lint/golint@latest
+  golint ./...
+
+  echo "---------"
+  echo "Running gosec..."
+
+  go install github.com/securego/gosec/v2/cmd/gosec@latest
+  gosec ./...
+}
+
+build_project() {
+  echo "Building project..."
+  go build -v -o bin/aw2 main.go
+}
+
+# Run the steps
+run_tests
+run_verification
+build_project
+
+echo "Build successful!"
+
